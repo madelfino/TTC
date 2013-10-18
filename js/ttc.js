4a962104be33689556f64ccb5ef6b7e3b6c241bb
Michael Delfino <michael.a.delfino@gmail.com>
2013-10-17 23:09:54 -0400

    Enforce whose turn it is

--- js/ttc.js
+++ js/ttc.js
@@ -1,13 +1,27 @@
 var DEBUG = false,
-    board;
+    board,
+    turn;
 
 var onDrop = function(source, target, piece, newPos, oldPos, orient) {
-    if (DEBUG) $('#title').html('source: ' + source + '<br>target: ' + target + '<br>piece: ' + piece + '<br>newPos: ' + JSON.stringify(newPos) + '<br>oldPos: ' + JSON.stringify(oldPos) + '<br>orient: ' + orient);
+    if (DEBUG) $('#title').html('source: ' + source +
+                                '<br>target: ' + target +
+                                '<br>piece: ' + piece +
+                                '<br>newPos: ' + JSON.stringify(newPos) +
+                                '<br>oldPos: ' + JSON.stringify(oldPos) +
+                                '<br>orient: ' + orient +
+                                '<br>turn: ' + turn +
+                                '<br>piece[0]: ' + piece[0]);
     for (square1 in newPos) {
         for (square2 in newPos) {
             if (newPos[square1] == newPos[square2] && square1 != square2)
                 return 'snapback';
         }
+    }
+    if (piece[0] != turn || JSON.stringify(newPos) == JSON.stringify(oldPos)) return 'snapback';
+    if (turn == 'w') {
+        turn = 'b';
+    } else {
+        turn = 'w';
     }
 }; //end onDrop()
 
@@ -17,6 +31,7 @@ var init = function() {
         sparePieces: true,
         onDrop: onDrop
     });
+    turn = 'w';
 }; // end init()
 
 $(document).ready(init);
