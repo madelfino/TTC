23178c8e5dede4023b0fb7c6c31bc9c45d43eb4c
Michael Delfino <michael.a.delfino@gmail.com>
2013-10-17 22:44:24 -0400

    Disallow adding multiple instances of the same piece

--- js/ttc.js
+++ js/ttc.js
@@ -1,7 +1,21 @@
+var DEBUG = false,
+    board;
+
+var onDrop = function(source, target, piece, newPos, oldPos, orient) {
+    if (DEBUG) $('#title').html('source: ' + source + '<br>target: ' + target + '<br>piece: ' + piece + '<br>newPos: ' + JSON.stringify(newPos) + '<br>oldPos: ' + JSON.stringify(oldPos) + '<br>orient: ' + orient);
+    for (square1 in newPos) {
+        for (square2 in newPos) {
+            if (newPos[square1] == newPos[square2] && square1 != square2)
+                return 'snapback';
+        }
+    }
+}; //end onDrop()
+
 var init = function() {
-    var board = new ChessBoard('board', {
+    board = new ChessBoard('board', {
         draggable: true,
-        sparePieces: true
+        sparePieces: true,
+        onDrop: onDrop
     });
 }; // end init()
 
